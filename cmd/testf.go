3ee8659285ff9dd1e3ec29c7f21f6e52b20c4eed
Masci, Richard (rx7322) <rx7322@us.att.com>
2022-12-09 12:21:16 -0500

    used Fprint instead so that the user can redirect messages accordingly.

--- cmd/testf.go
+++ cmd/testf.go
@@ -14,38 +14,38 @@ func main() {
 	pflag.BoolVarP(&verb.V, "verbose", "v", false, "Verbose Mode")
 	pflag.Parse()
 	i++
-	verb.Printf("Date Noline number. %d, %s\n", i, "Printf")
+	verb.Fprintf(os.Stdout, "Date Noline number. %d, %s\n", i, "Printf")
 	i++
-	verb.Println("Date No line number", i, "Println")
+	verb.Fprintln(os.Stdout, "Date No line number", i, "Println")
 	verb = verbose.New()
 	verb.PrintLine = true
 	i++
-	verb.Printf("line number. %d, %s\n", i, "Printf")
+	verb.Fprintf(os.Stdout, "line number. %d, %s\n", i, "Printf")
 	i++
-	verb.Println("line number", i, "Println")
+	verb.Fprintln(os.Stdout, "line number", i, "Println")
 	i++
-	verb.Print("line number. ", i, " Print\n")
+	verb.Fprint(os.Stderr, "line number. ", i, " Print\n")
 	i++
 
 	verb = verbose.New("date")
 	verb.V = true
 	verb.PrintLine = true
 	verb.Delimeter = "|"
-	verb.Printf("Date keyworkd no line number. %d, %s\n", i, "Printf")
+	verb.Fprintf(os.Stdout, "Date keyworkd no line number. %d, %s\n", i, "Printf")
 	i++
-	verb.Println("date keyword no line number", i, "Println")
+	verb.Fprintln(os.Stdout, "date keyword no line number", i, "Println")
 	i++
-	verb.Print("line number. ", i, " Print\n")
+	verb.Fprint(os.Stderr, "line number. ", i, " Print\n")
 	i++
 
 	verb = verbose.New()
 	verb.V = true
 	verb.PrintLine = true
-	verb.Printf("No Date  line number. %d, %s\n", i, "Printf")
+	verb.Fprintf(os.Stdout, "No Date  line number. %d, %s\n", i, "Printf")
 	i++
-	verb.Println("No Date line number", i, "Println")
+	verb.Fprintln(os.Stdout, "No Date line number", i, "Println")
 	i++
-	verb.Print("line number. ", i, " Print\n")
+	verb.Fprint(os.Stderr, "line number. ", i, " Print\n")
 	i++
 	fmt.Println("Done")
 	var z verbose.Verb
